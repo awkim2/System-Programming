bd16ff83f0a39f162f857bdd117147bd3239af3e
Meng Zhang <mengz5@illinois.edu>
2019-09-06 22:38:18 -0500

    9.6

--- extreme_edge_cases/camelCaser_ref_tests.c
+++ extreme_edge_cases/camelCaser_ref_tests.c
@@ -8,13 +8,14 @@
 
 int main() {
     // Enter the string you want to test with the reference here
-    char *input = "This is my string. ha ha ha!";
-
+    char *input = "7nG9k2vO8gmNL4yKcy1. kk81mqmKdWkpWgyL. GtxVB/C8Y370bA./6eZVh9I. o1jJ6VlkPlBJq.C,ke 6DZJVJe5U ..GnNT'Kjt4o1, 4jfeTzqlE5INinBj44eB0f!!! !ATZ8BvUM0xDWzgqeyFhEC RZhjo5kFD5VR0q7b, LfBMLhHOqW3n09.!ioviz9 LLMx. M05CaY3M/F2.  .uU47h8 kqTxEYZn d1MVh";
+  
     // This function prints the reference implementation output on the terminal
     print_camelCaser(input);
 
     // Put your expected output for the given input above
-    char **output = NULL;
+    //char **output = NULL;
+    char *output[] = {"hello", "welcomeToCS241", "iLoveSystemProgramming", NULL};
 
     // Compares the expected output you supplied with the reference output
     printf("check_output test: %d\n", check_output(input, output));
--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -20,5 +20,180 @@
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
     // TODO: Return 1 if the passed in function works properly; 0 if it doesn't.
+
+    // test NULL
+    const char* test_0 = NULL;
+    char** out_0 = camelCaser(test_0);
+    if(out_0!= NULL){
+        destroy(out_0);
+        return 0;
+    }
+    //test single punct
+    const char* pun = ".";
+    char** out_pun = camelCaser(pun);
+    if(out_pun[0][0] != '\0' || out_pun[1] != NULL){
+        destroy(out_pun);
+        return 0;
+    }
+
+    //test1
+    const char* test = "  . ..A.";
+    char** out = camelCaser(test);
+    char** except1 = malloc(5*sizeof(char*));
+    except1[0] = "";
+    except1[1] = "";
+    except1[2] = "";
+    except1[3] = "a";
+    except1[4] = NULL;
+
+    if((sizeof(except1)/sizeof(char*)) != (sizeof(test)/sizeof(char*))){
+        destroy(out);
+        free(except1);
+        return 0;
+    }
+
+    int iter  = 0;
+    while(except1[iter]){
+        if(strcmp(except1[iter], out[iter]) != 0){
+            destroy(out);
+            free(except1);
+            return 0;
+        }
+        iter++;
+    }
+    destroy(out);
+    free(except1);
+
+
+    //test2
+    test = "/32";
+    out = camelCaser(test);
+    char** except2 = malloc(2*sizeof(char*));
+    except2[0] = "";
+    except2[1] = NULL;
+
+    if((sizeof(except2)/sizeof(char*)) != (sizeof(test)/sizeof(char*))){
+        destroy(out);
+        free(except2);
+        return 0;
+    }
+
+    iter  = 0;
+    while(except2[iter]){
+        if(strcmp(except2[iter], out[iter]) != 0){
+            destroy(out);
+            free(except2);
+            return 0;
+        }
+        iter++;
+    }
+    destroy(out);
+    free(except2);
+    
+
+    //test3
+    test = "@a.$.";
+    out = camelCaser(test);
+    char** except3 = malloc(5*sizeof(char*));
+    except3[0] = "";
+    except3[1] = "a";
+    except3[2] = "";
+    except3[3] = "";
+    except3[4] = NULL;
+
+    if((sizeof(except3)/sizeof(char*)) != (sizeof(test)/sizeof(char*))){
+        destroy(out);
+        free(except3);
+        return 0;
+    }
+
+    iter  = 0;
+    while(except3[iter]){
+        if(strcmp(except3[iter], out[iter]) != 0){
+            destroy(out);
+            free(except3);
+            return 0;
+        }
+        iter++;
+    }
+    destroy(out);
+    free(except3);
+
+    //test4 
+    test = ".a...a.";
+    out = camelCaser(test);
+    char** except4 = malloc(6*sizeof(char*));
+    except4[0] = "";
+    except4[1] = "a";
+    except4[2] = "";
+    except4[3] = "";
+    except4[4] = "a";
+    except4[5] = NULL;
+
+    if((sizeof(except4)/sizeof(char*)) != (sizeof(test)/sizeof(char*))){
+        destroy(out);
+        free(except4);
+        return 0;
+    }
+
+    iter  = 0;
+    while(except4[iter]){
+        if(strcmp(except4[iter], out[iter]) != 0){
+            destroy(out);
+            free(except4);
+            return 0;
+        }
+        iter++;
+    }
+    destroy(out);
+    free(except4);
+
+    // //test5
+    // test = "7nG9k2vO8gmNL4yKcy1. kk81mqmKdWkpWgyL. GtxVB/C8Y370bA./6eZVh9I. o1jJ6VlkPlBJq.C,ke 6DZJVJe5U ..GnNT'Kjt4o1, 4jfeTzqlE5INinBj44eB0f!!! !ATZ8BvUM0xDWzgqeyFhEC RZhjo5kFD5VR0q7b, LfBMLhHOqW3n09.!ioviz9 LLMx. M05CaY3M/F2.  .uU47h8 kqTxEYZn d1MVh";
+    // out = camelCaser(test);
+    // char** except5 = malloc(24*sizeof(char*));
+    // except5[0] = "7ng9k2vo8gmnl4ykcy1";
+    // except5[1] = "kk81mqmkdwkpwgyl";
+    // except5[2] = "gtxvb";
+    // except5[3] = "c8y370ba";
+    // except5[4] = "";
+    // except5[5] = "6ezvh9i";
+    // except5[6] = "o1jj6vlkplbjq";
+    // except5[7] = "c";
+    // except5[8] = "ke6Dzjvje5u";
+    // except5[9] = "";
+    // except5[10] = "gnnt";
+    // except5[11] = "kjt4o1";
+    // except5[12] = "4jfetzqle5ininbj44eb0f";
+    // except5[13] = "";
+    // except5[14] = "";
+    // except5[15] = "";
+    // except5[16] = "atz8bvum0xdwzgqeyfhecRzhjo5kfd5vr0q7b";
+    // except5[17] = "lfbmlhhoqw3n09";
+    // except5[18] = "";
+    // except5[19] = "ioviz9Llmx";
+    // except5[20] = "m05cay3m";
+    // except5[21] = "f2";
+    // except5[22] = "";
+    // except5[23] = NULL;
+
+    // if((sizeof(except5)/sizeof(char*)) != (sizeof(test)/sizeof(char*))){
+    //     destroy(out);
+    //     free(except5);
+    //     return 0;
+    // }
+
+    // iter  = 0;
+    // while(except5[iter]){
+    //     if(strcmp(except5[iter], out[iter]) != 0){
+    //         destroy(out);
+    //         free(except5);
+    //         return 0;
+    //     }
+    //     iter++;
+    // }
+    // destroy(out);
+    // free(except5);
+
     return 1;
 }
