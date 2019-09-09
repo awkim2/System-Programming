c466e848202d648ec86f192297d5de700a44ce73
Maciej Krzysiak <mkrzys2@illinois.edu>
2019-09-08 23:10:28 -0500

    Deploying vector

---
+++ vector/sstring_test.c
@@ -0,0 +1,10 @@
+/**
+ * Vector
+ * CS 241 - Fall 2019
+ */
+#include "sstring.h"
+
+int main(int argc, char *argv[]) {
+    // TODO create some tests
+    return 0;
+}
---
+++ vector/vector_test.c
@@ -0,0 +1,9 @@
+/**
+ * Vector
+ * CS 241 - Fall 2019
+ */
+#include "vector.h"
+int main(int argc, char *argv[]) {
+    // Write your test cases here
+    return 0;
+}
