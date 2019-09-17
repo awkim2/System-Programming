3f064266b4fd980f00c4fb65355ada833f14ce35
Meng Zhang <mengz5@illinois.edu>
2019-09-16 21:52:20 -0500

    mon

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -1,50 +1,72 @@
 /**
  * Utilities Unleashed
  * CS 241 - Fall 2019
+ * partner: ruoyuz3 mengz5
  */
 
 
 #include "format.h"
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
-// char** split(char* ele){
-//     char** res = calloc(2, sizeof(char*));
-//     char* first = ele;
-//     char* sec = ele;
-
-// }
+char **strsplit(const char* input, const char* d) {
+    char *s = strdup(input);
+    size_t tokens_alloc = 1;
+    size_t tokens_used = 0;
+    char **tokens = calloc(tokens_alloc, sizeof(char*));
+    char *token = s;
+    char *rest = s;
+    while ((token = strsep(&rest, d))) {
+        if (tokens_used == tokens_alloc) {
+            tokens_alloc *= 2;
+            tokens = realloc(tokens, tokens_alloc * sizeof(char*));
+        }
+        tokens[tokens_used++] = strdup(token);
+    }
+    if (tokens_used == 0) {
+        free(tokens);
+        tokens = NULL;
+    } else {
+        tokens = realloc(tokens, tokens_used * sizeof(char*));
+    }
+    free(s);
+    return tokens;
+}
 
 
 int main(int argc, char *argv[]) {
     if(argc < 3) print_env_usage();
-
-
-
-
-    // int pid = fork();
-    // if(pid < 0) print_fork_failed();
-    // if(pid == 0){
-    //     //child
-    //     int exce1 = execvp();
-    //     if(exce1 == -1) print_exec_failed();
-    //     int pid2 = fork();
-    //     if(pid2 < 0) print_fork_failed();
-    //     if(pid == 0){
-    //         int exce2 = execvp();
-    //         if(exce2 == -1) print_exec_failed();
-    //         exit(1);
-    //     }else{
-    //         int status2 = 0;
-    //         waitpid(pid2, $status2, 0);
-    //     }
-    //     exit(1);
-    // }else{
-    //     int status = 0;
-    //     waitpid(pid, &status, 0);
-
-    // }
+    int flag = 0;
+    pid_t pid = fork();
+    if(pid == -1){
+        print_fork_failed();
+    }
+    else if(pid == 0){
+        //child 
+        int i = 1;
+        for(; i< argc-2; i++){
+            if(strcmp(argv[i],"--") == 0) break;
+            char** splited = strsplit(argv[i], "=");
+            if(splited[1][0] == '%'){
+                char*c = getenv(splited[1]+1);
+                flag = setenv(splited[0],c,1);
+                if(flag == -1) print_environment_change_failed();
+            } else{
+                flag = setenv(splited[0],splited[1],1);
+                if(flag == -1) print_environment_change_failed();
+            }
+            free(splited);
+        }
+        execvp(argv[i+1],&argv[i+1]);
+        print_exec_failed();
+    }else{
+        int status;
+        waitpid(pid,&status,0);
+        return 0;
+    }
     return 0;
 }
