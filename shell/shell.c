f7128a21d2cefdf197c7156dd7b75b03a132ad16
Meng Zhang <mengz5@illinois.edu>
2019-09-18 23:22:12 -0500

    wed

--- shell/shell.c
+++ shell/shell.c
@@ -5,13 +5,48 @@
 #include "format.h"
 #include "shell.h"
 #include "vector.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+extern char *optarg;
+extern int optind, opterr, optopt;
+
 
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
 
+
+
+
+
 int shell(int argc, char *argv[]) {
     // TODO: This is the entry point for your shell.
+
+    // vector* history = string_vector_create();
+    // char* history_file = NULL;
+
+    //bad input
+    if(!(argc == 1 || argc == 3 || argc == 5)){
+        print_usage();
+        exit(1);
+    }
+
+
+    char cwd[PATH_MAX+1];
+    int pid = getpid();
+    //char input[1024];
+
+        //Prompting input 
+        if (getcwd(cwd, sizeof(cwd))){
+            print_prompt(cwd, pid);
+        }
+
+
+
     return 0;
 }
