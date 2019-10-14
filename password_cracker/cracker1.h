a3784d7158a52d94d4ade6ce174caf53dd737604
Krzysiak <mkrzys2@illinois.edu>
2019-10-13 21:27:09 -0500

    Deploying password_cracker

---
+++ password_cracker/cracker1.h
@@ -0,0 +1,13 @@
+/**
+ * Password Cracker
+ * CS 241 - Fall 2019
+ */
+/**
+ * this file exists so that we can provide a main method which does a variety of
+ * handy things
+ */
+
+#include <stddef.h>
+
+// entry point for student code
+int start(size_t thread_count);
