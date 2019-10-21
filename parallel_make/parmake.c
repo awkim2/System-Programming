447263e12ef972da58c4e32ced973138c37816ae
Aarya Chandhok <aaryac2@illinois.edu>
2019-10-21 01:45:12 -0500

    Parmake Posted Precariously

---
+++ parallel_make/parmake.c
@@ -0,0 +1,15 @@
+/**
+ * Parallel Make Lab
+ * CS 241 - Fall 2019
+ */
+ 
+
+#include "format.h"
+#include "graph.h"
+#include "parmake.h"
+#include "parser.h"
+
+int parmake(char *makefile, size_t num_threads, char **targets) {
+    // good luck!
+    return 0;
+}
