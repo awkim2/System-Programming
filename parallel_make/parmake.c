eb52af4fbc6176e711c066ae08913c61c45cd3f0
Meng Zhang <mengz5@illinois.edu>
2019-10-24 22:58:05 -0500

    111

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -9,7 +9,13 @@
 #include "parmake.h"
 #include "parser.h"
 
+queue *q = NULL; 
+graph* g = NULL;
+
 int parmake(char *makefile, size_t num_threads, char **targets) {
     // good luck!
+    q = queue_create(-1);
+    g = parser_parse_makefile(makefile, targets);
+    vector *goal = graph_neighbors(g, "");
     return 0;
 }
