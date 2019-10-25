1265eb3f2924834f9cccc87ab89c1120d8a9d1fb
Meng Zhang <mengz5@illinois.edu>
2019-10-24 22:51:05 -0500

    thur2

---
+++ parallel_make/.objs/parmake-release.d
@@ -0,0 +1,21 @@
+.objs/parmake-release.o: parmake.c format.h includes/graph.h \
+ includes/callbacks.h includes/compare.h includes/dictionary.h \
+ includes/vector.h parmake.h parser.h rule.h
+
+format.h:
+
+includes/graph.h:
+
+includes/callbacks.h:
+
+includes/compare.h:
+
+includes/dictionary.h:
+
+includes/vector.h:
+
+parmake.h:
+
+parser.h:
+
+rule.h:
