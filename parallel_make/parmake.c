b210569b477c80ac6db0d500a29d417f7a6bab31
Meng Zhang <mengz5@illinois.edu>
2019-10-27 22:46:15 -0500

    sun

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -42,7 +42,7 @@ int isCyclic(char* target) {
     visited_set = NULL;
     return 0;
 }
-int fail_check(char* target){
+int is_fail(char* target){
     int flag = 0;
     if(access(target, F_OK) == -1) flag = 1;
     vector* neighbor_vec = graph_neighbors(g,target);
@@ -57,17 +57,18 @@ int fail_check(char* target){
             stat(neighbor, &newtime);
             if (difftime(oldtime.st_mtime, newtime.st_mtime) < 0) flag = 0;
         }
-        fail_check(neighbor);   
+        is_fail(neighbor);   
     }
+    vector_destroy(neighbor_vec);
     rule_t *rule = (rule_t*) graph_get_vertex_value(g, target);
     if(flag && !rule->state && !fail){
         queue_push(q, rule);
         rule_t *rule = queue_pull(q);
         for (size_t i = 0; i < vector_size(rule->commands); i++) {
-        if (system(vector_get(rule->commands, i)) != 0) {
-            fail = 1;
-            break;
-        }
+            if (system(vector_get(rule->commands, i)) != 0) {
+                fail = 1;
+                break;
+            }
         }   
         rule->state = 1;
     }
@@ -82,7 +83,7 @@ int parmake(char *makefile, size_t num_threads, char **targets) {
     for(size_t i = 0; i < vector_size(goal_vec); i++){
         char* goal =  vector_get(goal_vec, i);
         if(isCyclic(goal)) print_cycle_failure(goal);
-        else {fail_check(goal);}
+        else {is_fail(goal);}
     }
     queue_push(q,NULL);
     vector_destroy(goal_vec);
