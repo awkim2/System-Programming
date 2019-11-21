032e5b5c566c82102a831e34f5d6d51f00176224
Meng Zhang <mengz5@illinois.edu>
2019-11-20 18:37:37 -0600

    t

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,6 +4,8 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
@@ -11,6 +13,59 @@
   ./lookup1 <data_file> <word> [<word> ...]
 */
 
+int binarySearch(FILE* file, char* word, long offset){
+    if(offset == 0) return 0;
+    fseek(file, offset, SEEK_SET);
+    BinaryTreeNode node;
+    fread(&node, sizeof(BinaryTreeNode), 1, file);
+    fseek(file, offset + sizeof(BinaryTreeNode), SEEK_SET);
+    char wordInFile[20];
+    fread(wordInFile, 20, 1, file);
+    if (strcmp(word, wordInFile) == 0){
+      printFound(wordInFile, node.count, node.price);
+      return 1;
+    }
+    if (strcmp(word, wordInFile) < 0){
+      if (binarySearch(file,  word, node.left_child)){
+        return 1;
+      }
+    }
+    if (strcmp(word, wordInFile) > 0){
+      if (binarySearch(file,  word, node.right_child)){
+        return 1;
+      }
+    }
+    return 0;
+}
+
 int main(int argc, char **argv) {
+    if( argc < 3){
+      printArgumentUsage();
+      exit(1);
+    }
+    char* input = argv[1];
+    FILE* file = fopen(input, "r");
+    if(!file){
+      openFail(input);
+      exit(2);
+    }
+    //long pos = ftell(file);
+    char buffer[4];
+    fread(buffer, 1, 4, file);
+    //printf("first 4 byte is %s\n", buffer);
+    if(strcmp(buffer, "BTRE") != 0){
+      formatFail(input);
+      exit(2);
+    }
+    for(int i = 2; i < argc; i++){
+      int res = binarySearch(file, argv[i], (long)4);
+      if( res == 0){
+        printNotFound(argv[i]);
+      }
+    }
+    //fseek(file, (long) 4, SEEK_CUR);
+    //long newpos = ftell(file);
+    //printf("old pos is %ld, new pos is  %ld\n", pos, newpos);
+    fclose(file);
     return 0;
 }
--- mad_mad_access_patterns/lookup2.c
+++ mad_mad_access_patterns/lookup2.c
@@ -4,6 +4,13 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 /*
   Look up a few nodes in the tree and print the info they contain.
@@ -11,7 +18,53 @@
 
   ./lookup2 <data_file> <word> [<word> ...]
 */
+BinaryTreeNode *binarySearch(uint32_t offset, char *add, char *word) {
+  if(offset == 0) return NULL;
+  BinaryTreeNode* node = (BinaryTreeNode *) (add + offset);
+  BinaryTreeNode* child;
+  if (strcmp(word, node->word) == 0) return node;
+  if (strcmp(word, node->word) < 0) {
+    if ((child = binarySearch(node->left_child, add, word))) return child;
+  }
+  if (strcmp(word, node->word) > 0) {
+    if ((child = binarySearch(node->right_child, add, word))) return child;
+  }
+  return NULL;
+}
 
 int main(int argc, char **argv) {
+    if( argc < 3){
+      printArgumentUsage();
+      exit(1);
+    }
+    char* input = argv[1];
+    int fd = open(input, O_RDONLY);
+    if(fd < 0) {
+      openFail(input);
+      exit(2);
+    }
+    struct stat stats;
+    if(fstat(fd, &stats) !=0) {
+      openFail(input);
+      exit(2);
+    }
+    char* add = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (add == (void *)-1) {
+      mmapFail(input);
+      exit(2);
+    }
+    if (strncmp(add, "BTRE", 4)) {
+      formatFail(input);
+      exit(2);
+    }
+    for (int i = 2; i < argc; i++) {
+      BinaryTreeNode *node = binarySearch(4, add, argv[i]);
+      if (node) {
+        printFound(node->word, node->count, node->price);
+      } else {
+        printNotFound(argv[i]);
+      }
+    }
+    close(fd);
     return 0;
 }
